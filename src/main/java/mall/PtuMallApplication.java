a3183286bdd07e6ad60904984364defd8d691b23
rae <mail@web.sb>
2023-05-15 10:14:04 +0800

    initial commit.

---
+++ src/main/java/mall/PtuMallApplication.java
@@ -0,0 +1,11 @@
+package mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtuMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PtuMallApplication.class,args);
+    }
+}
